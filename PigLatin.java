1db710448202041ab519a307786927c86bd5b47c
JesseStuy20 <jxie20@stuy.edu>
2021-01-04 13:40:52 -0500

    PigLatin Part I works only for words that don't start with a vowel, and not working with uppercase letters right now

--- PigLatin.java
+++ PigLatin.java
@@ -8,7 +8,17 @@ public class PigLatin {
         Scanner n = new Scanner(in.nextLine()) ;
         while (n.hasNext()) {
           String temp = n.next() ;
-          System.out.print(temp + "ay") ;
+          String returnValue = temp ;
+          if (!(((temp.charAt(0)+"").equals("a")) || ((temp.charAt(0)+"").equals("e")) || ((temp.charAt(0)+"").equals("i"))
+          || ((temp.charAt(0)+"").equals("o")) || ((temp.charAt(0)+"").equals("u")))) {
+            returnValue = "" ;
+            for (int i=1; i<temp.length(); i++) {
+              returnValue = returnValue + temp.charAt(i) ;
+            }
+            returnValue = returnValue + temp.charAt(0) ;
+            returnValue = returnValue + "ay" ;
+          }
+          System.out.print(returnValue) ;
           System.out.print(" ") ;
         }
         System.out.println() ;
