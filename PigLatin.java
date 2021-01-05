2e81cd365b429874ea62c2696fa388ae2b7e0d8c
JesseStuy20 <jxie20@stuy.edu>
2021-01-05 13:57:55 -0500

    Fixed pigLatinSimple to be a method and edited main

--- PigLatin.java
+++ PigLatin.java
@@ -2,36 +2,38 @@ import java.util.Scanner ;
 public class PigLatin {
 
   public static void main(String[] args) {
-
-      Scanner in = new Scanner(System.in) ;
-      while (in.hasNextLine()) {
-        Scanner n = new Scanner(in.nextLine()) ;
-        while (n.hasNext()) {
-          String temp = n.next().toLowerCase() ;
-          String returnValue = temp ;
-          String[] vowels = {"a","e","i","o","u"};
-          boolean vowelCheck = false ;
-          for (int i=0; i<5; i++) {
-            if (vowels[i].equals(temp.charAt(0)+"")) {
-              vowelCheck = true ;
-            }
-          }
-          if (vowelCheck) {
-            returnValue = returnValue + "hay" ;
-          }
-          else {
-            returnValue = "" ;
-            for (int i=1; i<temp.length(); i++) {
-              returnValue = returnValue + temp.charAt(i) ;
-            }
-            returnValue = returnValue + temp.charAt(0) ;
-            returnValue = returnValue + "ay" ;
-          }
-          System.out.print(returnValue) ;
-          System.out.print(" ") ;
-        }
-        System.out.println() ;
+    Scanner in = new Scanner(System.in) ;
+    while (in.hasNextLine()) {
+      Scanner n = new Scanner(in.nextLine()) ;
+      while (n.hasNext()) {
+        System.out.print(pigLatinSimple(n.next()).toLowerCase()) ;
       }
+      System.out.println() ;
+    }
+  }
+
+  public static String pigLatinSimple(String s) {
+      String temp = s ;
+      String returnValue = temp ;
+      String[] vowels = {"a","e","i","o","u"};
+      boolean vowelCheck = false ;
+      for (int i=0; i<5; i++) {
+        if (vowels[i].equals(temp.charAt(0)+"")) {
+          vowelCheck = true ;
+        }
+      }
+      if (vowelCheck) {
+        returnValue = returnValue + "hay" ;
+      }
+      else {
+        returnValue = "" ;
+        for (int i=1; i<temp.length(); i++) {
+          returnValue = returnValue + temp.charAt(i) ;
+        }
+        returnValue = returnValue + temp.charAt(0) ;
+        returnValue = returnValue + "ay" ;
+      }
+      return returnValue + " " ;
     }
 
 }
