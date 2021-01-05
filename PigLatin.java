11fc2c6efd97d41bb5fef11e7497bc24e3ddd812
JesseStuy20 <jxie20@stuy.edu>
2021-01-05 16:01:42 -0500

    Added pigLatinBest method and edited main

--- PigLatin.java
+++ PigLatin.java
@@ -7,7 +7,8 @@ public class PigLatin {
       Scanner n = new Scanner(in.nextLine()) ;
       while (n.hasNext()) {
         // System.out.print(pigLatinSimple(n.next()).toLowerCase()) ;
-        System.out.print(pigLatin(n.next().toLowerCase())) ;
+        //System.out.print(pigLatin(n.next().toLowerCase())) ;
+        System.out.print(pigLatinBest(n.next().toLowerCase())+" ") ;
       }
       System.out.println() ;
     }
@@ -33,7 +34,7 @@ public class PigLatin {
         returnValue = returnValue + s.charAt(0) ;
         returnValue = returnValue + "ay" ;
       }
-      return returnValue + " " ;
+      return returnValue ;
     }
 
   public static String pigLatin(String s) {
@@ -74,7 +75,38 @@ public class PigLatin {
         returnValue = returnValue + s.charAt(0) ;
         returnValue = returnValue + "ay" ;
       }
-      return returnValue + " " ;
+      return returnValue ;
+    }
+
+    public static String pigLatinBest(String s) {
+      boolean letCheck = false ;
+      String lets[] = {"a","b","c","d","e","f","g","h","i","j","k",
+    "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",} ;
+      for (int i=0; i<26; i++) {
+        if ((s.charAt(0)+"").equals(lets[i])) {
+          letCheck = true ;
+        }
+      }
+      if (!(letCheck)) {
+        return s ;
+      }
+      else {
+        String punct = "" ;
+        boolean charScan = false ;
+        int lastNum = (s.length()-1) ;
+        for(int i=0; i<26; i++) {
+          if ((s.charAt(lastNum)+"").equals(lets[i])) {
+            charScan = true ;
+          }
+        }
+        String returnValue = s ;
+        if (!(charScan)) {
+          punct = (s.charAt(lastNum)+"") ;
+          returnValue = s.substring(0,lastNum) ;
+        }
+        returnValue = pigLatin(returnValue)+punct ;
+        return returnValue ;
+      }
     }
 
 }
